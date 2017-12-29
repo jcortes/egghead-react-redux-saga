24513be266a5aeabc4ff605d8ed15f326cab4a03
Jorge Cortes <jcortes@belatrixsf.com>
2017-12-29 11:22:45 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Redux Saga</h1>
+        <div>
+          {
+            this.props.starWars.people.map(
+              (person, idx) =>
+                <h4 key={idx}>{person.name}</h4>
+            )
+          }
+        </div>
+        <button>Load More</button>
+      </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
+    );
+  }
+}
+
+export default App;
